5947d5ce3c53cba6d5a4878872b37e6403074f13
Bolbol <134863700+BelboulLeFurieux@users.noreply.github.com>
2026-01-11 14:38:14 +0100

    Update create_tables.sql
    begining of table's creation

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -1,1 +1,14 @@
+CREATE DATABASE country_analysis
 
+CREATE TABLE continent (
+continentId INT,
+name VARCHAR(20),
+  );
+
+
+
+CREATE TABLE country
+
+CREATE TABLE index
+
+CREATE TABLE country_index
